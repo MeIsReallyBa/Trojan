15c8518f78ee38d3a8caaff7716120d15cd92035
MeIsReallyBa <38341209+MeIsReallyBa@users.noreply.github.com>
2020-02-29 12:16:33 +0800

    Update Trojan.sh

--- Trojan.sh
+++ Trojan.sh
@@ -208,7 +208,7 @@ EOF
 {
     "run_type": "server",
     "local_addr": "0.0.0.0",
-    "local_port": 443,
+    "local_port": 44321,
     "remote_addr": "127.0.0.1",
     "remote_port": 80,
     "password": [
